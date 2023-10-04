2997559b33c1c8d18c5c3a3caafd7bad601459a5
Enrico Daga <enrico.daga@open.ac.uk>
2023-10-04 16:25:35 +0100

    Exposed validator

--- src/reeco/__init__.py
+++ src/reeco/__init__.py
@@ -5,6 +5,7 @@ Python library with utility functions for the REECO Framework.
 # __init__.py
 
 # Version of the reeco package
-__version__ = "0.0.2-1696432778"
+__version__ = "0.0.2-1696432903"
 
 from .schema import Schema
+from .validator import Validator
