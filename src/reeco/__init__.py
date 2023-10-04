e2a30ec6a7e473cd1fb5d85c84849d709ebfa98b
Enrico Daga <enrico.daga@open.ac.uk>
2023-10-04 15:47:18 +0100

    Update version

--- src/reeco/__init__.py
+++ src/reeco/__init__.py
@@ -5,6 +5,6 @@ Python library with utility functions for the REECO Framework.
 # __init__.py
 
 # Version of the reeco package
-__version__ = "0.0.2"
+__version__ = "0.0.2-1"
 
 from .schema import Schema
