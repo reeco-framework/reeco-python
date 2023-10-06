f286bd786b48361b31e403734d715829ece8e93e
Enrico Daga <enrico.daga@open.ac.uk>
2023-10-06 11:17:15 +0100

    incremental build and release - 1696587414

--- src/reeco/__init__.py
+++ src/reeco/__init__.py
@@ -5,7 +5,7 @@ Python library with utility functions for the REECO Framework.
 # __init__.py
 
 # Version of the reeco package
-__version__ = "0.0.2-1696432903"
+__version__ = "0.0.2-1696587414"
 
 from .schema import Schema
 from .validator import Validator
